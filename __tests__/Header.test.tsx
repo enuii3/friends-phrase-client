9cb4e41948c9f5e31fc3b385d578e2f81f554312
enuii3 <b.nxxxi551@gmail.com>
2022-05-24 05:13:51 +0900

    delete: testの削除

--- __tests__/Header.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Header from '../components/organisms/Header'
-
-it('Should render hello text', () => {
-  render(<Header />)
-  expect(screen.getByText('FriendsPhrase')).toBeInTheDocument()
-})
