3a7ad3b40c22173e761f13d2ef25b50eaf8d0185
enuii3 <b.nxxxi551@gmail.com>
2022-06-17 00:34:24 +0900

    delete: PhraseCardListコンポーネントの廃止

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,27 @@ import Layout from '../components/templates/Layout'
 import { GetStaticProps } from 'next'
 import { getAllPhrasesData } from '../lib/fetch'
 import { Phrases } from '../types/types'
-import PhraseCardList from '../components/organisms/PhraseCardList'
+import PhraseCard from '../components/molecules/PhraseCard'
+import CardCase from '../components/atoms/CardCase'
+import Link from 'next/link'
 
 const MainPage: React.VFC<Phrases> = ({ phrases }) => {
   return (
     <Layout title="FriendsPhrase">
-      <PhraseCardList phrases={phrases} />
+      <ul className="w-full">
+        {phrases &&
+          phrases.map((phrase) => (
+            <li key={phrase.id}>
+              <Link href={`/phrases/${phrase.id}`}>
+                <a>
+                  <CardCase hover={true}>
+                    <PhraseCard phrase={phrase} />
+                  </CardCase>
+                </a>
+              </Link>
+            </li>
+          ))}
+      </ul>
     </Layout>
   )
 }
